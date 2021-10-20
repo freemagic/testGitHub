af97681d7eb9eb5fa87e7643e91d1afa51e5f72d
liyinuo <liyinuo@021.com>
2021-10-20 15:14:34 +0800

    初始化项目

---
+++ src/com/xyz/lyn/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.xyz.lyn;
+
+/**
+ * @author ：liyinuo
+ * @date ：Created in 2021-10-20 15:13
+ * @description：测试GitHub
+ * @version: 1.0$
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
